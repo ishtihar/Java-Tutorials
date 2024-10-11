c4802eb1fc61503610a35a558bd62d76b771a567
Ishtihar Khan <ishtihar9@gmail.com>
2024-10-11 17:27:55 +0530

    Update ArrayPractice.java

--- src/Arrays/ArrayPractice.java
+++ src/Arrays/ArrayPractice.java
@@ -1,6 +1,6 @@
 package Arrays;
 import java.util.Scanner;
-//ishtiharkhan 1995-2024hour
+//ishtiharkhan 1995-2024hour hudsdsddsdsd
 public class ArrayPractice {
     public static void main(String[] args) {
 //        Create an array of 5 floats and calculate their sum.
