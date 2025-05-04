b6f066a9602576adf2f4d0369372c7e64f0e74f3
ishtihar <ishtihar9@gmail.com>
2025-05-04 16:20:22 +0530

    encap 4

---
+++ src/Oops/Encapsulation_ex4.java
@@ -0,0 +1,39 @@
+package Oops;
+
+class Bank1  {
+    private double balance;
+
+
+    public Bank1(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if(balance>=0){
+            this.balance = balance;
+
+        }
+        else {
+            System.out.println("Balance can't negative");
+
+        }
+
+    }
+}
+
+
+public class Encapsulation_ex4 {
+    public static void main(String[] args) {
+        Bank1 bank1 = new Bank1(4000);
+        System.out.println("Current balance" +" "+ bank1.getBalance());
+        bank1.setBalance(10000);
+        System.out.println("updated balance" + " " + bank1.getBalance());
+        bank1.setBalance(-3322);
+        System.out.println(bank1.getBalance());
+
+
+    }}
